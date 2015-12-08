81739835ca0314aa92391b16d97de2b07943181b
Kodlee Yin <kodlee@kodleeshare.net>
2015-12-08 13:27:06 -0800

    Basic spotify auth and usage added

---
+++ js/spotify_auth.js
@@ -0,0 +1,14 @@
+(function() {
+	$('#login').click(function() {
+		var client_id = '431b90e60b8149be99c537b32ebd269f'; // Your client id
+		var redirect_uri = 'http://343-project.local/favorites.html'; // Your redirect uri
+
+		var scope = 'user-follow-modify user-follow-read';
+		var url = 'https://accounts.spotify.com/authorize';
+		url += '?response_type=token';
+		url += '&client_id=' + encodeURIComponent(client_id);
+		url += '&scope=' + encodeURIComponent(scope);
+		url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+		window.location = url;
+	});
+} ());
