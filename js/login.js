e903bb0f2c9b542fb2c54db0a0cfb9693b3f3990
Kodlee Yin <kodlee@kodleeshare.net>
2015-12-09 22:02:49 -0800

    Cleaned up and fixed login for remote server

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@
 $(function() {
 	$('#login').click(function() {
 		var client_id = '431b90e60b8149be99c537b32ebd269f'; // Your client id
-		var redirect_uri = 'http://i343-project.local/auth'; // Your redirect uri
+		var redirect_uri = 'http://343-project.fru1t.me/auth'; // Your redirect uri
 
 		var scope = 'user-follow-modify user-follow-read';
 		var url = 'https://accounts.spotify.com/authorize';
